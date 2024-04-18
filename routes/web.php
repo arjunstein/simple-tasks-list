ff9b57a673e7deae508bcc9786daf33786ba850a
arjunstein <arjun.gnw09@gmail.com>
2024-04-18 11:17:03 +0700

    🎉 initial commit

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => Task::latest()->get()
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', ['task' => Task::findOrFail($id)]);
+})->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|min:3|max:255',
+        'description' => 'required|string|min:3',
+        'long_description' =>  'required|string|min:5',
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
+})->name('tasks.store');
+
+// Route::fallback(function () {
+    // return 'Not found page';
+// });
