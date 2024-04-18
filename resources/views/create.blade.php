83c833eab1dc89ecbbc8288e5a0a0f99fd55b7c2
arjunstein <arjun.gnw09@gmail.com>
2024-04-18 12:41:28 +0700

    ✨ session, errors, and flash message

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,8 +2,16 @@
 
 @section('title', 'Add Task')
 
+@section('styles')
+<style>
+    .error-message {
+        color: #ff0000;
+        font-size: 14px;
+    }
+    </style>
+@endsection
+
 @section('content')
-{{ $errors }}
     <form action="{{ route('tasks.store') }}" method="post">
         @csrf
         <div>
@@ -11,18 +19,27 @@
                 Title
             </label>
             <input type="text" name="title" id="title">
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <label for="description">
                 Description
             </label>
             <textarea name="description" id="description" rows="5"></textarea>
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <label for="long_description">
                 Long Description
             </label>
             <textarea name="long_description" id="long_description" rows="10"></textarea>
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
         <div><button type="submit">Add Task</button></div>
     </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Task List App</title>
+    @yield('styles')
 </head>
 
 <body>
     <h2>@yield('title')</h2>
     <div>
+        @if (session()->has('success'))
+        <div>{{ session('success') }}</div>
+        @endif
         @yield('content')
     </div>
 </body>
